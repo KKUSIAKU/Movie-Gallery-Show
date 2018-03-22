99b8333494c98422d7a3bbed967d672770a00dd7
KKUSIAKU <kusiaku.k@gmail.com>
2018-03-22 19:51:42 +0000

    recheck video route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 var hompePage = require("./routes/homepage.js"),
-  videosRoute = require("/routes/videos.js");
+  videosRoute = require("./routes/videos.js");
 
 module.exports = function (app) {
   hompePage(app);
