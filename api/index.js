5c3cb76e7680a1ead9658a88b70266a5556c3ec7
KKUSIAKU <kusiaku.k@gmail.com>
2018-03-23 17:34:22 +0000

    add search route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 var hompePage = require("./routes/homepage.js"),
-  videosRoute = require("./routes/videos.js");
+  videosRoute = require("./routes/videos.js"),
+  searchRoute = require("./routes/search");
 
-module.exports = function (app,database) {
-  videosRoute(app,database);
+module.exports = function (app, database) {
+  videosRoute(app, database);
+  searchRoute(app, database);
   hompePage(app);
   //videosRoute(app);
 }; 
