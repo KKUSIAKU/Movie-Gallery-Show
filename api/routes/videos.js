7ba5ab5115df2fb17f4f5f1564261569521eb433
KKUSIAKU <kusiaku.k@gmail.com>
2018-03-23 16:50:36 +0000

    ch7

--- api/routes/videos.js
+++ api/routes/videos.js
@@ -1,14 +1,37 @@
 
 module.exports = function (app, database) {
 
-
-
   // check perfomance on poster query and to avoid skip
   app.get("/videos", function (req, res) {
-    var db;
+    var db, page, cursor, limit = 25, result = {};
 
     try {
       db = database.db("video");
+      page = req.query.page ? req.query.page : 0;
+
+      cursor = db.collection("movieDetails")
+        .find({ poster: { $ne: null } },
+          { _id: 0, title: 1, poster: 1, genres: 1, plot: 1, year: 1 })
+        .skip(limit * (parseInt(page))).sort({ _id: -1 }).limit(limit);
+
+      // put count and getGenres in parrallel operations
+      cursor.count(false)
+        .then(function onCountFulfilled(data) {
+          result.numberOfResult = data;
+        })
+        .then(function onCountFinished() {
+          return cursor.toArray();
+        })
+        .then(function onCursorToArrayFulFilled(data) {
+          result.movies = data;
+          result.page = page;
+          res.json(result);
+        })
+
+        .catch(function onCursorError(err) {
+          res.send(`error ${err.message}`);
+          //console.log("error in movies fetching promise chain", err.message);
+        });
     }
     catch (e) {
       res.send(`error ${e.message}  ${database}`);
