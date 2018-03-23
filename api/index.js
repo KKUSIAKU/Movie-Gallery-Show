b1fc3723044e0c5565e1ae223ff9d5b550e6b91d
KKUSIAKU <kusiaku.k@gmail.com>
2018-03-23 16:43:50 +0000

    ch6

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 var hompePage = require("./routes/homepage.js"),
   videosRoute = require("./routes/videos.js");
 
-module.exports = function (app) {
-  videosRoute(app);
+module.exports = function (app,database) {
+  videosRoute(app,database);
   hompePage(app);
   //videosRoute(app);
 }; 
