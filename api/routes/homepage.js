1b94a750327b3033b623686750a03aa8f034fd51
KKUSIAKU <kusiaku.k@gmail.com>
2018-03-23 17:16:29 +0000

    ch12

--- api/routes/homepage.js
+++ api/routes/homepage.js
@@ -3,14 +3,13 @@ var path = require("path");
 var rootDirectory = require("../../config.js").rootDirectory;
 
 module.exports = function (app) {
-
+/*
   app.get("/",function(req,res){
     res.send("hello from page");
   })
- 
-  /*
+ */
+  
   app.use(express.static(path.resolve(rootDirectory, "docs")));
   app.use(express.static(path.resolve(rootDirectory, "src", "public", "images")));
 
-  */
 };
--- api/routes/videos.js
+++ api/routes/videos.js
@@ -1,6 +1,5 @@
 
 module.exports = function (app, database) {
-
   // check perfomance on poster query and to avoid skip
   app.get("/videos", function (req, res) {
     var db, page, cursor, limit = 25, result = {};
@@ -26,57 +25,16 @@ module.exports = function (app, database) {
           result.movies = data;
           result.page = page;
           res.json(result);
-          ///res.send(" Hello from videos routes");
         })
 
         .catch(function onCursorError(err) {
+          // set error message
           res.send(`error ${err.message}`);
-          //console.log("error in movies fetching promise chain", err.message);
         });
     }
     catch (e) {
-      res.send(`error ${e.message}  ${database}`);
-    }
-
-   
-
-
-
-
-
-
-    // 
-
-    //var db = database.db("video"), limit = 25, result = {};
-    //res.send(" routes videos");
-
-    //var page = req.query.page ? req.query.page : 0;
-    /*
-    var cursor = db.collection("movieDetails")
-      .find({ poster: { $ne: null } },
-        { _id: 0, title: 1, poster: 1, genres: 1, plot: 1, year: 1 })
-      .skip(limit * (parseInt(page))).sort({ _id: -1 }).limit(limit);
-
-    // put count and getGenres in parrallel operations
-    cursor.count(false)
-      .then(function onCountFulfilled(data) {
-        result.numberOfResult = data;
-      })
-      .then(function onCountFinished() {
-        return cursor.toArray();
-      })
-      .then(function onCursorToArrayFulFilled(data) {
-        result.movies = data;
-        result.page = page;
-        res.json(result);
-      })
-     
-      .catch(function onCursorError(err) {
-        console.log("error in movies fetching promise chain", err.message);
-      });
-      */
-    //res.send(" routes videos");
-
+      // need to check consistency with promise catch above
+      res.send(`error ${e.message}`);
+    } 
   });
-
 };
