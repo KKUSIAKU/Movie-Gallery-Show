64136a0f92c0b793864392416c27785604aeb376
KKUSIAKU <kusiaku.k@gmail.com>
2018-04-04 11:28:03 +0100

    after server.js recheck

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-start:npm run start
+start:node server
--- server.js
+++ server.js
@@ -2,36 +2,36 @@ var express = require("express"),
   path = require("path"),
   assert = require("assert"),
   bodyParser = require("body-parser"),
-  MongoClient = require("mongodb").MongoClient;;
+  MongoClient = require("mongodb").MongoClient;
 
 var app = express(),
   uri = process.env.MGDB_ACCES_URI;
 //app.use(bodyParser.json()); 
 //app.use(bodyParser.urlencoded({extended:true}));
 
-app.set("PORT", (process.env.PORT || 8010));
+app.set("PORT", (process.env.PORT || 5000));
 
 //app.use(express.static(path.resolve(__dirname, "docs")));
 
 //require("./api")(app);
 
-
 MongoClient.connect(uri, function (err, database) {
-  
+
   assert.equal(null, err);
   assert.ok(database);
-
-  require("./api")(app, database); 
+  require("./api")(app, database);
 
   app.listen(app.get("PORT"), function () {
     if (process.env.NODE_ENV !== "production") {
       console.log("app listent on", app.get("PORT"));
     }
   });
-  
+
 });
 
+
 /*
+
 app.get("/", function (req, res) {
   var port = process.env;
   res.send(port);
