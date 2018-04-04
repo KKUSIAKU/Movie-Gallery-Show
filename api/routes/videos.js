c131e53b7c835c809e69d682c91c30aecf3b0a79
KKUSIAKU <kusiaku.k@gmail.com>
2018-04-04 10:28:30 +0100

    add projection obj to root path

--- api/routes/videos.js
+++ api/routes/videos.js
@@ -10,7 +10,7 @@ module.exports = function (app, database) {
 
       cursor = db.collection("movieDetails")
         .find({ poster: { $ne: null } },
-          { _id: 0, title: 1, poster: 1, genres: 1, plot: 1, year: 1 })
+          { projection: { _id: 0, title: 1, poster: 1, genres: 1, plot: 1, year: 1 } })
         .skip(limit * (parseInt(page))).sort({ _id: -1 }).limit(limit);
 
       cursor.count(false)
@@ -34,6 +34,6 @@ module.exports = function (app, database) {
     catch (e) {
       // need to check consistency with promise catch above
       res.send(`error ${e.message}`);
-    } 
+    }
   });
 };
